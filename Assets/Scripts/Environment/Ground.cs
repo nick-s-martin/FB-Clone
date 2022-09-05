ff234126b37373ba804536a93a9688e7ae7cebd9
Nick Martin <111902885+nick-s-martin@users.noreply.github.com>
2022-09-05 14:49:57 +0100

    Added ground with script

---
+++ Assets/Scripts/Environment/Ground.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Scripts.Environment
+{
+    public class Ground : MonoBehaviour
+    {
+        [SerializeField] Transform _transform;
+
+        void Update()
+        {
+        if (_transform.position.x < -4.5)
+            {
+                _transform.position += new Vector3(9, 0, 0);
+            }
+        }
+    }
+
+}
